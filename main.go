e80ce2cce315e1d40fccaf931fffb46c4da8bf36
kepung <kepung@gmail.com>
2018-04-17 21:27:42 +1200

    implemented secret validation

--- main.go
+++ main.go
@@ -10,10 +10,16 @@ import (
 	"strconv"
 )
 
-func YourHandler(w http.ResponseWriter, r *http.Request) {
+var secretGlobal string
+
+func GenerateKey(w http.ResponseWriter, r *http.Request) {
 
 	totp := GenerateQr("issuer", "account", 30)
+
+	secretGlobal = totp.Secret()
+
 	fmt.Print(totp.Secret())
+
 	img, _ := totp.Image(100,100)
 
 	buffer := new(bytes.Buffer)
@@ -26,14 +32,25 @@ func YourHandler(w http.ResponseWriter, r *http.Request) {
 	if _, err := w.Write(buffer.Bytes()); err != nil {
 		log.Println("unable to write image.")
 	}
+}
 
-	//w.Write([]byte("Gorilla!\n"))
+func ValidateUserKey(w http.ResponseWriter, r *http.Request) {
+
+	key := r.URL.Query()["key"]
+
+	fmt.Println("key", key)
+	fmt.Println("secretGlobal", secretGlobal)
+
+	result := Validate(key[0], secretGlobal)
+	fmt.Println(result)
+
 }
 
 func main() {
 	r := mux.NewRouter()
 	// Routes consist of a path and a handler function.
-	r.HandleFunc("/", YourHandler)
+	r.HandleFunc("/generatekey", GenerateKey)
+	r.HandleFunc("/validate", ValidateUserKey)
 
 	// Bind to a port and pass our router in
 	log.Fatal(http.ListenAndServe(":8000", r))
